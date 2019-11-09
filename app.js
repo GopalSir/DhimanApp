b7c80c63fd4e1b98d50a2a9e801bc74961cb2120
Gaurav Dhiman <46110945+GopalSir@users.noreply.github.com>
2019-11-09 13:13:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const http = require('http');
+var fs = require('fs');
+
 const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello Ashish</h1>');
+
+
+http.createServer(function(req,res){
+fs.readFile("./Public/index.html","UTF-8",function(err,html){
+    res.writeHead(200,{"Content-Type": "text/html"});
+    res.end(html);
 });
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
+
+}).listen(port);
